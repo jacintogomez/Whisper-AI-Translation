e2a934860ea6ceef3d4f3eb35e9f70eb6a399495
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2024-04-11 20:45:53 -0400

    Reformatting

--- speech-to-text.py
+++ speech-to-text.py
@@ -10,7 +10,7 @@ client = OpenAI()
 
 audio_file=open("recordings/speech.mp3", "rb")
 transcription = client.audio.transcriptions.create(
-  model="whisper-1",
-  file=audio_file
+    model="whisper-1",
+    file=audio_file
 )
 print(transcription.text)
--- text-to-speech.py
+++ text-to-speech.py
@@ -1,19 +1,22 @@
 # text input, audio file output
 
-from pathlib import Path
 from openai import OpenAI
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
-openai_api_key=os.getenv("OPENAI_API_KEY")
+openai_api_key = os.getenv("OPENAI_API_KEY")
 client = OpenAI()
 
-speech_file_path = Path(__file__).parent / "span.mp3"
+speech_file_path = "recordings/span.mp3"
+
 response = client.audio.speech.create(
-  model="tts-1",
-  voice="alloy",
-  input="hola, cómo estás"
+    model="tts-1",
+    voice="alloy",
+    input="我不说英语，你呢？"
 )
 
-response.stream_to_file(speech_file_path)
+with open(speech_file_path, "wb") as f:
+    f.write(response.content)
+
+print("Speech file saved successfully!")
